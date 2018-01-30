8dbc0c715b051792a9ac5bc6f41490693ed42992
Pedro Arias Perez <parias@alpha29.aulas.gsyc.urjc.es>
2018-01-30 15:47:18 +0100

    Primera version

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 
 import sys
-import math
+import operator
 
-print(sys.argv)
-print(sys.argv[1])
-print(math.pi)
+operations = {'suma': operator.add, 'resta': operator.sub,
+            'multiplicar': operator.mul, 'dividir': operator.truediv}
+
+if len(sys.argv) != 4:
+    print('Usage error: <operation> <num1> <num2>')
+    sys.exit()
+
+try:
+    op = operations[sys.argv[1]]
+except KeyError:
+    print('Invalid operation')
+    sys.exit()
+
+print(op(int(sys.argv[2]), int(sys.argv[3])))
