7597e99800cf95db5d69064b9cfc30127f591c8f
pariaspe <pedroariasperez96@gamil.com>
2018-02-02 19:43:27 +0100

    Sexta versión

--- calculadora.py
+++ calculadora.py
@@ -15,11 +15,10 @@ if len(sys.argv) != 4:
 
 try:
     op = operations[sys.argv[1]]
-except KeyError:
+    print(op(int(sys.argv[2]), int(sys.argv[3])))
+except (KeyError, ValueError):
     print('Invalid operation')
     sys.exit()
-
-# ZeroDivisionError
-# ValueError
-
-print(op(int(sys.argv[2]), int(sys.argv[3])))
+except ZeroDivisionError:
+    print('Can not be divided by zero')
+    sys.exit()
